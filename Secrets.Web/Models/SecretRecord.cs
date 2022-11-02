a4b5f56afd5e7243a0bb8b9d29581df0a18d871e
Divine Bicycle <divine-bicycle@proton.me>
2022-11-02 09:56:41 +0800

    Created the SecretRecord model to represent a record from the database

---
+++ Secrets.Web/Models/SecretRecord.cs
@@ -0,0 +1,21 @@
+using System.Text.Json.Serialization;
+
+namespace Secrets.Web.Models;
+
+public class SecretRecord
+{
+	[JsonInclude] public string Passphrase;
+	[JsonInclude] public string Secret;
+	[JsonInclude] public DateTime BackupDate;
+	[JsonInclude] public char FavouriteLetter;
+	[JsonInclude] public short FavouriteNum;
+	
+	public SecretRecord(string passphrase, string secret, DateTime backupDate, char favouriteLetter, short favouriteNum)
+	{
+		Passphrase = passphrase;
+		Secret = secret;
+		BackupDate = backupDate;
+		FavouriteLetter = favouriteLetter;
+		FavouriteNum = favouriteNum;
+	}
+}
