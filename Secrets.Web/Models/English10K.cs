3554ed16e33dedd876a1b31085be0ab3e301de7f
Divine Bicycle <divine-bicycle@proton.me>
2022-11-02 10:19:26 +0800

    Created model for word list, and implemented posting a SecretRecord to the api for it to be added to the database

---
+++ Secrets.Web/Models/English10K.cs
@@ -0,0 +1,9 @@
+namespace Secrets.Web.Models;
+
+public class English10K
+{
+	public string Name { get; set; }
+	public bool LeftToRight { get; set; }
+	public bool NoLazyMode { get; set; }
+	public string[] Words { get; set; }
+}
