e212492797a25595d7e6a58a497a5068f7befc3a
Divine Bicycle <divine-bicycle@proton.me>
2022-11-02 13:11:33 +0800

    Implement the GET api and fixed the port in FetchSecret.razor

--- Secrets.API/Program.cs
+++ Secrets.API/Program.cs
@@ -1,20 +1,32 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.Data;
+using System.Text.Json;
+using Secrets.API.Models;
+using MySqlConnector;
 
-// Add services to the container.
+WebApplicationBuilder _builder = WebApplication.CreateBuilder(args);
+_builder.Services.AddCors(policyBuilder => policyBuilder.AddDefaultPolicy(policy => policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+WebApplication _app = _builder.Build();
 
-var app = builder.Build();
+string _connectionString = File.ReadAllText(@"/home/erickth/Documents/Code/CSharp/connectionstring.txt").Trim();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+_app.MapGet("/secrets/passphrase/{passphrase}", async (string passphrase) =>
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
-}
+	MySqlConnection _connection = new MySqlConnection(_connectionString);
+	await _connection.OpenAsync();
+
+	MySqlCommand _command = new MySqlCommand($"SELECT `Passphrase`, `Secret`, `BackupDate`, `FavouriteLetter`, `FavouriteNum`  FROM Secrets.prod WHERE Passphrase = '{passphrase}'", _connection);
+	return await ReadDb(_command);
+});
+
+_app.MapGet("/secrets/backup-data/{date}/{favouriteDigit}/{favouriteLetter}", async (string date, int favouriteDigit, char favouriteLetter) =>
+{
+	MySqlConnection _connection = new MySqlConnection(_connectionString);
+	await _connection.OpenAsync();
+	
+	MySqlCommand _command = new MySqlCommand($"SELECT `Passphrase`, `Secret`, `BackupDate`, `FavouriteLetter`, `FavouriteNum`  FROM Secrets.prod WHERE BackupDate = '{date}' AND FavouriteLetter = '{favouriteLetter}' AND FavouriteNum = '{favouriteDigit}'", _connection);
+	return await ReadDb(_command);
+});
 
 app.UseHttpsRedirection();
 
@@ -22,4 +34,16 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+async Task<IResult> ReadDb(MySqlCommand command)
+{
+	MySqlDataReader _reader = await command.ExecuteReaderAsync();
+	bool _success = await _reader.ReadAsync();
+	if (!_success) { return Results.NotFound(); }
+
+	SecretRecord _record = new SecretRecord(_reader.GetString(0), _reader.GetString(1), _reader.GetDateTime(2), _reader.GetChar(3), _reader.GetInt16(4));
+
+	return Results.Ok(_record);
+}
+
+_app.UseCors();
+_app.Run();
